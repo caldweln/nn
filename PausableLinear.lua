38b447466c19adbece629507acf6100d90520bbd
nick <nick.caldwell94@gmail.com>
2016-04-13 18:15:55 +0200

    fix: pass self to Parent call

--- PausableLinear.lua
+++ PausableLinear.lua
@@ -6,20 +6,20 @@ function PausableLinear:__init(inputSize, outputSize)
    self.pause = 0 -- pos value to prevent accGradParameters/updateParameters
 end
 
-function PausableLinear:pause()
-  self.pause = 1
-end
-
-function PausableLinear:resume()
-  self.pause = 0
-end
-
 function PausableLinear:accGradParameters(input, gradOutput, scale)
   if self.pause > 0 then return end -- do nothing
-  Parent.accGradParameters(input, gradOutput, scale)
+  Parent.accGradParameters(self, input, gradOutput, scale)
 end
 
 function PausableLinear:updateParameters(learningRate)
   if self.pause > 0 then return end -- do nothing
   Parent.updateParameters(self, learningRate)
+end
+
+function PausableLinear:__tostring__()
+  local pauseStr = ''
+  if self.pause > 0 then pauseStr = ' paused' end
+
+  return torch.type(self) ..
+      string.format('(%d -> %d)'..pauseStr, self.weight:size(2), self.weight:size(1))
 end
