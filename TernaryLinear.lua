c11729b3fc31ed6a2f66c5f7cb89ca01468d7f01
nick <nick.caldwell94@gmail.com>
2016-05-02 14:57:47 +0200

    TernaryLinear, only impl updateOutput (without bias) and ternarizes the output based on given thresholds

---
+++ TernaryLinear.lua
@@ -0,0 +1,49 @@
+local TernaryLinear, parent = torch.class('nn.TernaryLinear', 'nn.Module')
+
+function TernaryLinear:__init(inputSize, outputSize, outputThresholdHighs, outputThresholdLows)
+   parent.__init(self)
+
+   self.weight = torch.Tensor(outputSize, inputSize)
+   self.outputThresholdHighs = outputThresholdHighs
+   self.outputThresholdLows = outputThresholdLows
+end
+
+function TernaryLinear:updateOutput(input)
+
+  -- sum(input * weights)
+  if input:dim() == 1 then
+    self.output:resize(self.weight:size(1)):zero()
+    self.output:addmv(1, self.weight, input)
+  elseif input:dim() == 2 then
+    local nframe = input:size(1)
+    local nElement = self.output:nElement()
+    self.output:resize(nframe, self.weight:size(1))
+    if self.output:nElement() ~= nElement then
+       self.output:zero()
+    end
+    self.output:addmm(0, self.output, 1, input, self.weight:t())
+  else
+    error('input must be vector or matrix')
+  end
+
+  -- ternarize output
+  for i=1,self.output:size(1) do
+   self.output[i]:map2(self.outputThresholdHighs, self.outputThresholdLows,
+     function(outputVal, inThreshHigh, inThreshLow)
+       if outputVal > inThreshHigh then
+         return 1
+       elseif outputVal < inThreshLow then
+         return -1
+       else
+         return 0
+       end
+     end)
+  end
+
+  return self.output
+end
+
+function TernaryLinear:__tostring__()
+  return torch.type(self) ..
+      string.format('(%d -> %d)', self.weight:size(2), self.weight:size(1))
+end
